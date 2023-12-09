d330c3d46e8b58046389e4a5dd2a76106521efd7
Rafaella Sampaio <sampaiorafaella@hotmail.com>
2023-12-09 03:57:30 -0300

    add Swagger config

--- src/main/java/com/smd/todeolho/TodeolhoApplication.java
+++ src/main/java/com/smd/todeolho/TodeolhoApplication.java
@@ -3,11 +3,20 @@ package com.smd.todeolho;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @SpringBootApplication
 public class TodeolhoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TodeolhoApplication.class, args);
 	}
-
+   @Bean
+   public OpenAPI usersMicroserviceOpenAPI() {
+       return new OpenAPI()
+               .info(new Info().title("Tô de Olho")
+                                .description("API do sistema para monitoramento de obras públicas inacabadas")
+                                .version("1.0"));
+   }
 }
